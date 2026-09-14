60e672b6f9820115ee5ca3fe8184693dbe2c8e13
leelah-jaffer <jafferleelah6@gmail.com>
2026-09-14 09:00:40 +0200

    incorporated the styling to mainscreen.tsx

--- components/MainScreen.tsx
+++ components/MainScreen.tsx
@@ -1,59 +1,79 @@
-import {StatusBar} from 'expo-status-bar'
-import {useState} from 'react';
-import { View, SafeAreaView, ScrollView, Image, Text, TextInput} from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { useState } from 'react';
+import { View, SafeAreaView, ScrollView, Image, Text, TextInput } from 'react-native';
 import styles from '../components/Styles';
-import {RadioButton} from 'react-native-paper';
+import { RadioButton } from 'react-native-paper';
 
 
 
-function MainScreen (){
+function MainScreen() {
     const [petName, setPetName] = useState('');
     const [selectedValue, setSelectedValue] = useState('0');
-    
-    return(
+
+    return (
         <View>
             <SafeAreaView>
                 <ScrollView>
-                     <Image style={styles.logo}
-                     source={require('../_images/logo.jpg')}/>
+                    <Image style={styles.logo}
+                        source={require('../_images/logo.jpg')} />
 
-                     <Text style={styles.mainTxt}>Mog Hub</Text>
-                     <Text style={styles.slogan}>P u r r f e c t  C o m p a n i o n s</Text>
+                    <Text style={styles.mainTxt}>Mog Hub</Text>
+                    <Text style={styles.slogan}>P u r r f e c t  C o m p a n i o n s</Text>
 
-                     <View style={styles.inputFlex}>
+                    <View style={styles.inputFlex}>
                         <Text style={styles.enterTxt}>Your Pet's Name:</Text>
                         <TextInput style={styles.userInputTxt}
                             placeholder='Saidee'
-                             value={petName}
-                             onChangeText={newText => setPetName(newText)}
+                            value={petName}
+                            onChangeText={newText => setPetName(newText)}
                         />
-                     </View>
+                        </View>
 
-                 <View style={styles.radioContainer}>
-                    <View style={styles.radioGroup}>
-                        <View style={styles.radioButton}>
-                            <RadioButton.Android
-                            value="1"
-                            status={selectedValue == "1" ? 'checked' : 'unchecked'}
-                            onPress={() => setSelectedValue('1')}
-                              color="orange"
 
-                            />  
 
-                            
-                       </View>
-                    </View>
-                
-                 </View>
+                        <View style={styles.radioContainer}>
+                            <View style={styles.radioGroup}>
+                                <View style={styles.radioButton}>
+                                    <RadioButton.Android
+                                        value="1"
+                                        status={selectedValue == "1" ? 'checked' : 'unchecked'}
+                                        onPress={() => setSelectedValue('1')}
+                                        color="orange"
 
-                     
+                                    />
+                                </View>
+                                </View>
 
+
+
+                                <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+                                    <Text style={{ fontWeight: 'bold', fontSize: 18 }}>
+                                        Select your Pet:
+                                    </Text>
+
+                                    <View style={styles.radioContainer}>
+                                        <View style={styles.radioGroup}>
+                                            <View>
+
+
+                                            </View>
+
+
+                                        </View>
+
+
+                                    </View>
+
+
+
+                                </View>
+
+
+                            </View>
                 </ScrollView>
             </SafeAreaView>
         </View>
 
     )
-
 }
-
-export default MainScreen;
+                    export default MainScreen;
